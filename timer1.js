9c989fa219ea3ffab675a2e622a0e81ccfbb81d2
Brian LaFond <brian.d.lafond@gmail.com>
2021-08-11 16:39:08 +0000

    comments added

--- timer1.js
+++ timer1.js
@@ -1,8 +1,9 @@
-const args = process.argv.slice(2);
+const args = process.argv.slice(2); //allows entry from command line
 
-const timer = function(entries) {
-  for (let entry of entries) {
+const timer = function(entries) { 
+  for (let entry of entries) { //loops each command line entry, setTimout for each entry
     setTimeout(() => {
+      //process.stdout.write('\x07');
       process.stdout.write(`......${entry * 1000}`);
       process.stdout.write('\n');
     }, Number(entry * 1000)); //Number(entry) = parse entry argument into a number data type for setInterval syntax readability
@@ -10,3 +11,5 @@ const timer = function(entries) {
 };
 
 timer(args);
+
+//using '......${entry}' because beep ('\x07') did not work.
